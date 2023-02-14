f0167e92464c906854a042e5f7bff53d66b75580
Mayank Kashyap <107757811+Memomer@users.noreply.github.com>
2023-02-14 23:25:49 +0530

    Delete birthday-paradox-simulation.py

--- birthday-paradox-simulation.py
+++
@@ -1,5 +0,0 @@
-# Birthday paradox
-Did you know that the chances of finding 2 or more people having the same birthday is 97% if the number of people being considered are 28 and it goes higher as
-n increases.
-
-Discover the same by running the simulation mentioned here.
